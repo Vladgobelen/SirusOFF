c8e7994f04e422b30f84c60fbca338a042750f19
Vladgobelen <vladgobelen@gmail.com>
2023-03-17 20:11:34 +0700

    отмена путей

--- alarm1.sh
+++ alarm1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 env DISPLAY=:0
-mpv --force-window=no /home/diver/Загрузки/Музыка/12/111.mp3 &
+mpv --force-window=no ~/Музыка/Скрипты/alarm.mp3 &
 PID=$!
 trap 'kill -9 ${PID}' SIGTERM SIGKILL EXIT
 wait $PID
